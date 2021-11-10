85cf8c9694d8718d4631d885eed5a3932355f6ca
1sh1vam <shivam07.mt@gmail.com>
2021-11-10 20:54:08 +0530

    display restaurents and refactor the test for removing the repeated code

--- src/components/RestaurentList.js
+++ src/components/RestaurentList.js
@@ -1,11 +1,17 @@
 import {useEffect} from 'react';
 
-export const RestaurentList = ({loadRestaurents}) => {
+export const RestaurentList = ({loadRestaurents, restaurents}) => {
   useEffect(() => {
     loadRestaurents();
   }, [loadRestaurents]);
 
-  return <div>RestaurentList</div>;
+  return (
+    <ul>
+      {restaurents.map(restaurent => (
+        <li key={restaurent.id}>{restaurent.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default RestaurentList;
--- src/components/__tests__/RestaurentList.spec.js
+++ src/components/__tests__/RestaurentList.spec.js
@@ -2,10 +2,30 @@ import {render} from '@testing-library/react';
 import {RestaurentList} from '../RestaurentList';
 
 describe('RestaurentList', () => {
-  it('loads restaurents on first render', () => {
-    const loadRestaurents = jest.fn().mockName('loadRestaurents');
-    render(<RestaurentList loadRestaurents={loadRestaurents} />);
+  const restaurents = [
+    {id: 1, name: 'Salad Place'},
+    {id: 2, name: 'Pasta Place'},
+  ];
+  let loadRestaurents;
+  let context;
+  beforeEach(() => {
+    loadRestaurents = jest.fn().mockName('loadRestaurants');
+    context = render(
+      <RestaurentList
+        loadRestaurents={loadRestaurents}
+        restaurents={restaurents}
+      />,
+    );
+  });
 
+  it('loads restaurents on first render', () => {
     expect(loadRestaurents).toHaveBeenCalled();
+  });
+
+  it('display the restaurents', () => {
+    const {queryByText} = context;
+
+    expect(queryByText('Salad Place')).not.toBeNull();
+    expect(queryByText('Pasta Place')).not.toBeNull();
   });
 });

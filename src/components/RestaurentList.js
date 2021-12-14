6f057b23444ddfe59e6d79aa873fb5a3e5b5e7fd
1sh1vam <shivam07.mt@gmail.com>
2021-12-14 20:20:10 +0530

    setting up the restaurent list component

--- src/components/RestaurentList.js
+++ src/components/RestaurentList.js
@@ -1,21 +1,38 @@
 import {useEffect} from 'react';
 import {connect} from 'react-redux';
-import {List, ListItem, ListItemText} from '@material-ui/core';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  CircularProgress,
+} from '@material-ui/core';
+import {Alert} from '@material-ui/lab';
 import {loadRestaurents} from '../store/restaurents/actions';
 
-export const RestaurentList = ({loadRestaurents, restaurents}) => {
+export const RestaurentList = ({
+  loadRestaurents,
+  restaurents,
+  loading,
+  loadingError,
+}) => {
   useEffect(() => {
     loadRestaurents();
   }, [loadRestaurents]);
 
   return (
-    <List>
-      {restaurents.map(restaurent => (
-        <ListItem key={restaurent.id}>
-          <ListItemText>{restaurent.name}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {loading && <CircularProgress data-testid="loading-indicator" />}
+      {loadingError && (
+        <Alert severity="error">Restaurents could not be loaded.</Alert>
+      )}
+      <List>
+        {restaurents.map(restaurent => (
+          <ListItem key={restaurent.id}>
+            <ListItemText>{restaurent.name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
@@ -23,6 +40,8 @@ const mapDispatchToProps = {loadRestaurents};
 
 const mapStateToProps = state => ({
   restaurents: state.restaurents.records,
+  loading: state.restaurents.loading,
+  loadingError: state.restaurents.loadingError,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RestaurentList);

ec979fc0ee9213426d4662797cbc0f73cea957bf
1sh1vam <shivam07.mt@gmail.com>
2021-12-14 20:15:45 +0530

    added the test for ui render

--- src/components/__tests__/RestaurentList.spec.js
+++ src/components/__tests__/RestaurentList.spec.js
@@ -8,24 +8,65 @@ describe('RestaurentList', () => {
   ];
   let loadRestaurents;
   let context;
-  beforeEach(() => {
-    loadRestaurents = jest.fn().mockName('loadRestaurants');
-    context = render(
-      <RestaurentList
-        loadRestaurents={loadRestaurents}
-        restaurents={restaurents}
-      />,
-    );
-  });
+
+  const renderWithProps = (propOverrides = {}) => {
+    const props = {
+      loadRestaurents: jest.fn().mockName('loadRestaurants'),
+      restaurents,
+      loading: false,
+      loadingError: false,
+      ...propOverrides,
+    };
+
+    loadRestaurents = props.loadRestaurents;
+
+    context = render(<RestaurentList {...props} />);
+  };
 
   it('loads restaurents on first render', () => {
+    renderWithProps();
     expect(loadRestaurents).toHaveBeenCalled();
   });
 
-  it('display the restaurents', () => {
-    const {queryByText} = context;
+  describe('when loading succeeds', () => {
+    beforeEach(() => {
+      renderWithProps();
+    });
 
-    expect(queryByText('Salad Place')).not.toBeNull();
-    expect(queryByText('Pasta Place')).not.toBeNull();
+    it('display the restaurents', () => {
+      const {queryByText} = context;
+
+      expect(queryByText('Salad Place')).not.toBeNull();
+      expect(queryByText('Pasta Place')).not.toBeNull();
+    });
+
+    it('does not display loading indicator while not loading', async () => {
+      const {queryByTestId} = context;
+      expect(queryByTestId('loading-indicator')).toBeNull();
+    });
+
+    it('does not display the error messsage', () => {
+      const {queryByText} = context;
+      expect(queryByText('Restaurents could not be loaded.')).toBeNull();
+    });
   });
+
+  describe('when loading fails', () => {
+    beforeEach(() => {
+      renderWithProps({loadingError: true});
+    });
+
+    it('displays the error message', () => {
+      const {queryByText} = context;
+      expect(queryByText('Restaurents could not be loaded.')).not.toBeNull();
+    });
+  });
+
+  it('displays the loading indicator while loading', async () => {
+    renderWithProps({loading: true});
+    const {queryByTestId} = context;
+
+    expect(queryByTestId('loading-indicator')).not.toBeNull();
+  });
+
 });
